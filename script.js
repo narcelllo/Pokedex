d1f77b3c87fc5556d0a4abf603c1a54b903ec987
Mudeiasenha@5858 <marcello_magno@hotmail.com>
2023-05-15 22:32:35 -0300

    style add

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-for (let i = 1; i < 649; i++) {
+for (let i = 1; i <= 12; i++) {
     let image = document.createElement("img");
     image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/" + i + ".gif";
-    let div = document.getElementById("pokemon");
+    let div = document.createElement("div");
+    div.classList.add("pokeCard");
     div.appendChild(image);
+    let pokemon = document.getElementById("pokemon");
+    pokemon.appendChild(div);
 }
